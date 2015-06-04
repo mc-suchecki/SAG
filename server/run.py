ee9210a81b9f0ace013f5a656f7684cb207c9492
Maciej Suchecki <mc.suchecki@gmail.com>
2015-06-04 23:12:53 +0200

    Added initial directory tree structure

---
+++ server/run.py
@@ -0,0 +1,15 @@
+""" This is the main file of web server that is simulating stack exchange market. """
+__author__ = 'mc'
+
+import sys
+
+
+def main():
+    """ Main function of stack exchange simulation. """
+    # TODO initialize the market model
+    # TODO initialize the web server
+    # TODO run the simulation
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
