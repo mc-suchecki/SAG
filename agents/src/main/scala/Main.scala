c4c56c2777e33604406fe0984abbc9346b4bc25b
Jacek Witkowski <jacek.witkowski@gmail.com>
2015-06-06 16:46:59 +0200

    Added directory structure for agents project.

---
+++ agents/src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+/**
+ * Created by jacek on 5/24/15.
+ */
+sealed trait GroupAgentMsg
+case object Buy extends
+
+/*
+* scenariusz:
+* (adres do serwera)
+* - wylosuj liczbę pieniędzy jaką dysponujesz,
+* - zarejestruj się na serwerze,
+* [ustalenie kto jest masterem]
+* - czekaj na zlecenia od mastera i je realizuj.
+*
+* */
