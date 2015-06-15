7a241e4cf636e3437327bfbdc239300bebe93884
Maciej Suchecki <mc.suchecki@gmail.com>
2015-06-15 17:47:46 +0200

    Added backend for buy/sell offers placement

--- server/model/market.py
+++ server/model/market.py
@@ -1,3 +1,4 @@
+from model.offer import PurchaseOffer, SaleOffer
 from model.stock import Stock
 from model.trader import Trader
 import logging
@@ -11,11 +12,14 @@ class Market:
     """ Class representing the stock exchange market model. """
     stock = None
     traders = []
-    offers = []
+    purchase_offers = []
+    sale_offers = []
 
     def __init__(self):
-        self.traders = []
         self.stock = Stock(DEFAULT_TICKER)
+        self.traders = []
+        self.purchase_offers = []
+        self.sale_offers = []
 
     def add_trader(self):
         """ Adds new trader to the market. """
@@ -27,4 +31,24 @@ class Market:
     def get_stock_price(self, day):
         """ Returns historical stock price in a given day. """
         return self.stock.price_history[day]
+
+    def place_purchase_offer(self, trader_id, price, stocks):
+        """ Places an offer to buy a stock.
+        :param trader_id: ID of the trader that wants to buy a stock.
+        :param price: Proposed price of one stock.
+        :param stocks: Number of the stocks that the trader wants to buy.
+        """
+        offer = PurchaseOffer(trader_id, price, stocks)
+        self.purchase_offers.append(offer)
+        logging.info("New Offer registered! " + str(offer))
+
+    def place_sale_offer(self, trader_id, price, stocks):
+        """ Places an offer to purchase a stock.
+        :param trader_id: ID of the trader that wants to sell a stock.
+        :param price: Proposed price of one stock.
+        :param stocks: Number of the stocks that the trader wants to sell.
+        """
+        offer = SaleOffer(trader_id, price, stocks)
+        self.purchase_offers.append(offer)
+        logging.info("New Offer registered! " + str(offer))
 
--- server/web/server.py
+++ server/web/server.py
@@ -24,10 +24,12 @@ class WebServer():
         self.app.add_url_rule('/', 'main_page', self.main_page)
         self.app.add_url_rule('/traders', 'traders', self.register_trader, methods=['POST'])
         self.app.add_url_rule('/stock/price', 'price', self.get_stock_price, methods=['GET'])
+        self.app.add_url_rule('/stock/buy', 'buy', self.place_purchase_offer, methods=['POST'])
+        self.app.add_url_rule('/stock/sell', 'sell', self.place_sale_offer, methods=['POST'])
 
     def main_page(self):
-        """ Displays webpage with various statistics. """
-        return "Number of Traders registered so far: " + str(self.market.traders_count)
+        """ Displays webpage with simulation visualisation. """
+        return "Number of Traders registered so far: " + str(len(self.market.traders))
 
     def register_trader(self):
         """ Registers a new Trader - returns trader ID, cash amount and stocks number. """
@@ -38,3 +40,19 @@ class WebServer():
         """ Returns historical stock price. """
         day = request.args["day"]
         return jsonify({'price': float(self.market.get_stock_price(int(day)))})
+
+    def place_purchase_offer(self):
+        """ Allows traders to place offers to buy a stock. """
+        price = request.args["price"]
+        stocks = request.args["stocks"]
+        trader_id = request.args["trader_id"]
+        self.market.place_purchase_offer(trader_id, price, stocks)
+        return ""
+
+    def place_sale_offer(self):
+        """ Allows traders to place offers to sell a stock. """
+        price = request.args["price"]
+        stocks = request.args["stocks"]
+        trader_id = request.args["trader_id"]
+        self.market.place_sale_offer(trader_id, price, stocks)
+        return ""
